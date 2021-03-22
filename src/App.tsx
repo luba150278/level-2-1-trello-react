ddc60ab951928e45bb8e44f4c1682646e56f0074
Lyubov Myetolkina <luba150278@gmail.com>
2021-03-22 19:17:41 +0200

    try 22

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import Board from './pages/Board/Board';
 import Home from './pages/Home/Home';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <Navbar />
     <div className="container">
       <Switch>
         <Route component={Home} path="/" exact />
-        <Route component={Board} path="/board" exact />
         <Redirect exact to="/" />
       </Switch>
     </div>
